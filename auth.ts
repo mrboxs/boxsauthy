6b414b18211fb0515b24d4f102b5285616a979c3
mrboxs <rizkywanda200155@gmail.com>
2024-10-17 13:17:19 +0700

    feat: Add NextAuth configuration and routes
    - Add NextAuth configuration file and routes for authentication
    - Import necessary dependencies for authentication providers
    - Implement authentication logic using NextAuth
    - Create Prisma adapter for NextAuth
    - Add global Prisma client instance
    - Create middleware for authentication
    
    Closes #123

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import authConfig from "@/auth.config";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+
+import prisma from "@/lib/prisma";
+
+export const {
+  auth,
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+});
