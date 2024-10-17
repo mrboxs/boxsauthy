f6bde6c7ba158803a8048fb9db7b82f0385d425f
mrboxs <rizkywanda200155@gmail.com>
2024-10-17 18:13:15 +0700

    refactor: Update NextAuth configuration and callbacks to include user information in the token and session

--- auth.config.ts
+++ auth.config.ts
@@ -2,49 +2,29 @@ import type { NextAuthConfig } from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 
-import { getUserById } from "./data/user";
-
 export default {
   providers: [GitHub, Google],
   callbacks: {
-    async jwt({ token }) {
-      // Add access_token to the token right after signin
-      if (!token.sub) return token;
+    async jwt({ token, user, trigger, session }) {
+      if (trigger === "update") {
+        return { ...token, ...session.user };
+      }
 
-      // Fetch user from database
-      const existingUser = await getUserById(token.sub);
-      // If user exists, add it to the token
-      if (!existingUser) return token;
-
-      // extend the token with the user data
-      token.id = existingUser.id;
-      token.role = existingUser.roles;
-      token.username = existingUser.username;
-      token.verified = existingUser.verified;
+      if (user) {
+        token.sub = user.id;
+        token.role = user.role;
+        token.username = user.username;
+        token.verified = user.verified;
+      }
 
       return token;
     },
 
     async session({ token, session }) {
-      // Add property to session, like an access_token from a provider.
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-
-      // Add role to session
-      if (token.role && session.user) {
-        session.user.role = token.role;
-      }
-
-      // Add username to session
-      if (token.username && session.user) {
-        session.user.username = token.username;
-      }
-
-      // Add verified to session
-      if (token.verified && session.user) {
-        session.user.verified = token.verified === false;
-      }
+      session.user.id = token.id;
+      session.user.role = token.role;
+      session.user.username = token.username;
+      session.user.verified = token.verified;
 
       return session;
     },
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,25 +1,45 @@
-import { DefaultSession, DefaultUser } from "next-auth";
+// The `JWT` interface can be found in the `next-auth/jwt` submodule
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
+  /**
+   * The shape of the user object returned in the OAuth providers' `profile` callback,
+   * or the second parameter of the `session` callback, when using a database.
+   */
+  interface User {
+    id: string;
+    role: string;
+    username: string;
+    verified: boolean;
+  }
+  /**
+   * The shape of the account object returned in the OAuth providers' `account` callback,
+   * Usually contains information about the provider being used, like OAuth tokens (`access_token`, etc).
+   */
+  interface Account {
+    provider: string;
+    type: string;
+    id: string;
+    refreshToken: string;
+    accessToken: string;
+    accessTokenExpires: number;
+  }
+
+  /**
+   * Returned by `useSession`, `auth`, contains information about the active session.
+   */
   interface Session {
-    user?: {
-      id?: User["id"];
-      role?: User["role"];
-      username?: User["username"];
-      verified?: User["verified"];
-    } & DefaultSession["user"];
+    user: User;
   }
+}
 
-  interface User extends DefaultUser {
-    role?: (typeof User)["role"];
-    username?: (typeof User)["username"];
-    verified?: (typeof User)["verified"];
-  }
-
+declare module "next-auth/jwt" {
+  /** Returned by the `jwt` callback and `auth`, when using JWT sessions */
   interface JWT {
-    id?: string;
-    role?: (typeof User)["role"];
-    username?: (typeof User)["username"];
-    verified?: (typeof User)["verified"];
+    id: string;
+    role: string;
+    username: string;
+    verified: boolean;
   }
 }
